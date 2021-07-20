8e80de185b2242cb04bc133a46592a75e364eb69
Nicholas Yan <nicholasyan11@gmail.com>
2021-07-20 14:03:23 -0400

    lint; remove .eslintcache and add to .gitignore

--- src/lib/utils/splog.ts
+++ src/lib/utils/splog.ts
@@ -1,22 +1,22 @@
 import chalk from "chalk";
 
-export function logError(msg: string) {
+export function logError(msg: string): void {
   console.log(chalk.redBright(`ERROR: ${msg}`));
 }
 
-export function logErrorAndExit(msg: string) {
+export function logErrorAndExit(msg: string): void {
   logError(msg);
   process.exit(1);
 }
 
-export function logWarn(msg: string) {
+export function logWarn(msg: string): void {
   console.log(chalk.yellow(`WARNING: ${msg}`));
 }
 
-export function logInfo(msg: string) {
+export function logInfo(msg: string): void {
   console.log(chalk.blueBright(`${msg}`));
 }
 
-export function logSuccess(msg: string) {
+export function logSuccess(msg: string): void {
   console.log(chalk.green(`${msg}`));
 }
