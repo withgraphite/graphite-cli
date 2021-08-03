c54dbecedd3bce6c75b51977592112b6c04b778c
Nicholas Yan <nicholasyan11@gmail.com>
2021-08-03 13:44:41 -0400

    [Log] Initial MVP

--- src/commands/log.ts
+++ src/commands/log.ts
@@ -1,5 +1,8 @@
-import { execSync } from "child_process";
+import chalk from "chalk";
+import { printStack } from "../actions/print_stack";
 import { profiledHandler } from "../lib/telemetry";
+import { getTrunk } from "../lib/utils/trunk";
+import Branch from "../wrapper-classes/branch";
 
 const args = {} as const;
 
@@ -10,12 +13,46 @@ export const builder = args;
 export const handler = async (): Promise<void> => {
   return profiledHandler(command, async () => {
     try {
-      execSync(
-        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all`,
-        { stdio: "inherit" }
-      );
+      printTrunkLog();
+      await printStacksBehindTrunk();
     } catch (e) {
       // Ignore errors (this just means they quit git log)
     }
   });
 };
+
+function printTrunkLog(): void {
+  const trunk = getTrunk();
+  printStack(trunk, 0, {
+    currentBranch: Branch.getCurrentBranch(),
+    offTrunk: true,
+  });
+}
+
+async function printStacksBehindTrunk(): Promise<void> {
+  const trunk = getTrunk();
+  const branchesWithoutParents = (
+    await Branch.getAllBranchesWithoutParents()
+  ).filter((branch) => branch.name !== trunk.name);
+  if (branchesWithoutParents.length === 0) {
+    return;
+  }
+
+  console.log("․");
+  console.log(
+    `․ ${chalk.red(
+      `Stacks below trail ${trunk.name}. To fix, check out the stack and run \`gp stack fix\`.`
+    )}`
+  );
+  console.log("․");
+
+  branchesWithoutParents.forEach((branch) => {
+    console.log("․");
+    printStack(branch, 1, {
+      currentBranch: Branch.getCurrentBranch(),
+      offTrunk: false,
+    });
+    console.log(`◌──┘`);
+    console.log("․");
+  });
+}
