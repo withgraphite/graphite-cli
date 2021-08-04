a97fefb290efd9c5e8031494dc41c393ede11a54
Nicholas Yan <nicholasyan11@gmail.com>
2021-08-04 16:28:03 -0400

    update last time updated color

--- src/actions/print_stack.ts
+++ src/actions/print_stack.ts
@@ -76,7 +76,7 @@ function getBranchInfo(branch: Branch, config: TPrintStackConfig): string[] {
   const pr = prInfo !== undefined ? chalk.yellow(`PR #${prInfo.number}`) : "";
   branchInfo.push(`${dot} ${branchName} ${pr}`);
 
-  branchInfo.push(`│ ${chalk.gray(branch.lastUpdatedReadable())}`);
+  branchInfo.push(`│ ${chalk.dim(branch.lastUpdatedReadable())}`);
 
   return branchInfo;
 }
