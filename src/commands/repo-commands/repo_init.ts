629a7ab10c7dd495169b93c5fb745102229b3629
Merrill Lutsky <mlutsky1231@gmail.com>
2021-08-10 22:37:43 -0400

    Update src/commands/repo-commands/repo_init.ts
    Co-authored-by: Nicholas Yan <nicholasyan11@gmail.com>

--- src/commands/repo-commands/repo_init.ts
+++ src/commands/repo-commands/repo_init.ts
@@ -10,7 +10,7 @@ const args = {
     type: "string",
   },
   "ignore-branches": {
-    describe: `A list of branches that Graphite should ignore when constructing stacks (i.e. branches you never intend to merge into trunk).`,
+    describe: `A list of branches that Graphite should ignore when tracking your stacks (i.e. branches you never intend to merge into trunk).`,
     demandOption: false,
     optional: true,
     type: "string",
