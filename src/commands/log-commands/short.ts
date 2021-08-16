3751e1f27c9a9281e5a2df7c6381e0c72ff18fb8
Greg Foster <gregorymfoster@gmail.com>
2021-08-16 18:45:20 -0400

    refactor(log): move to noun verb pattern

--- src/commands/log-commands/short.ts
+++ src/commands/log-commands/short.ts
@@ -12,6 +12,10 @@ export const aliases = ["s"];
 type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 export const handler = async (argv: argsT): Promise<void> => {
   return profile(argv, async () => {
+<<<<<<< HEAD
     await stacksAction({ all: false, interactive: false });
+=======
+    await stacksAction(false, false);
+>>>>>>> 866e1b5 (refactor(log): move to noun verb pattern)
   });
 };
