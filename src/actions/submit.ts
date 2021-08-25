598c45db0e0e300386cdc05bdaf0697699a9d6e3
Greg Foster <gregorymfoster@gmail.com>
2021-08-25 09:29:16 -0400

    fix(error_logging): standardize ExitFailed errors

--- src/actions/submit.ts
+++ src/actions/submit.ts
@@ -104,9 +104,10 @@ function pushBranchesToRemote(branches: Branch[]): Branch[] {
           printStdout: true,
         },
       },
-      (_) => {
+      (err) => {
         throw new ExitFailedError(
-          `Failed to push changes for ${branch.name} to origin. Aborting...`
+          `Failed to push changes for ${branch.name} to origin. Aborting...`,
+          err
         );
       }
     )
@@ -225,7 +226,7 @@ async function submitPRsForBranches(args: {
       }).\n\nResponse: ${JSON.stringify(response)}`
     );
   } catch (error) {
-    throw new ExitFailedError(`Failed to submit PRs: ${error.message}`);
+    throw new ExitFailedError(`Failed to submit PRs`, error);
   }
 }
 
