92fdb500f7cc18b7393177cffe8ebd67847cc890
Nicholas Yan <nicholasyan11@gmail.com>
2021-09-23 16:31:21 -0400

    support b checkout and bco

--- src/commands/autocomplete.ts
+++ src/commands/autocomplete.ts
@@ -28,8 +28,9 @@ yargs.completion("completion", (current, argv) => {
  */
 function getBranchArg(current: string, argv: Arguments): string | null {
   const currentCommand = argv["_"].join(" ");
+
   // gt branch checkout --branch <branch_name>
-  if ((currentCommand.includes("branch checkout") || argv["_"].includes("bc")) && "branch" in argv) {
+  if ((currentCommand.includes("branch checkout") || currentCommand.includes("b checkout")) && "branch" in argv) {
     // Because --branch is an option on the overall command, we need to check
     // the value of current to make sure that the branch argument is the
     // current argument being entered by the user.
@@ -40,11 +41,20 @@ function getBranchArg(current: string, argv: Arguments): string | null {
     }
   }
 
+  // gt bco
+  // Check membership in argv to ensure that "bco" is its own entry (and not
+  // a substring of another command). Since we're dealing with a positional,
+  // we also want to make sure that the current argument is the positional
+  // (position 3).
+  if (argv["_"].includes("bco") && argv["_"].length <= 3 && typeof current === "string") {
+    return current;
+  }
+
   // gt upstack onto <branch_name>
   if (currentCommand.includes("upstack onto") || currentCommand.includes("us onto")) {
-    // Since we're detailing with a positional, we want to make sure that the
-    // current argument being entered is in the desired position, i.e. position
-    // 4.
+    // Again, since we're detailing with a positional, we want to make sure
+    // that the current argument being entered is in the desired position,
+    // i.e. position 4.
     if (argv["_"].length <= 4 && typeof current === "string") {
       return current;
     }
