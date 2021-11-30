d8959c563a50ecba933fc03782eb95b0a67d8760
Greg Foster <gregorymfoster@users.noreply.github.com>
2021-11-30 15:00:26 -0500

    feat(submit): add tip on branch submit (#445)

--- src/commands/branch-commands/submit.ts
+++ src/commands/branch-commands/submit.ts
@@ -1,12 +1,12 @@
 import { submitAction } from "../../actions/submit";
 import { profile } from "../../lib/telemetry";
+import { logTip } from "../../lib/utils";
 import type { argsT } from "../shared-commands/submit";
 
 export { aliases, args, builder, command } from "../shared-commands/submit";
 export const description =
   "Idempotently force push the current branch to GitHub, creating or updating a pull request.";
 export const canonical = "branch submit";
-
 
 export const handler = async (argv: argsT): Promise<void> => {
   await profile(argv, canonical, async () => {
@@ -16,5 +16,14 @@ export const handler = async (argv: argsT): Promise<void> => {
       createNewPRsAsDraft: argv.draft,
       dryRun: argv["dry-run"],
     });
+    logTip(
+      [
+        `You submitted a pull request for a specific branch.`,
+        `In common cases, we recommend you use:`,
+        `* 'gt stack submit'`,
+        `* 'gt downstack submit'`,
+        `because these will ensure any downstack changes will be synced to existing PRs.`,
+      ].join("\n")
+    );
   });
 };
