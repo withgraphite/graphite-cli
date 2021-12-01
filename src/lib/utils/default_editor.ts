f682023b3deb059bc6055254775bb571d4b71cb2
Neha Srivastava <6138253+neha-sri@users.noreply.github.com>
2021-12-01 10:51:06 -0500

    feat(submit): set editor if not set

--- src/lib/utils/default_editor.ts
+++ src/lib/utils/default_editor.ts
@@ -1,4 +1,8 @@
 import { gpExecSync } from "./exec_sync";
+import {logInfo} from "./splog";
+import chalk from "chalk";
+import prompts from "prompts";
+import {KilledError} from "../errors";
 
 export function getDefaultEditor(): string {
   const gitEditor = gpExecSync({ command: `echo \${GIT_EDITOR}` })
@@ -14,4 +18,62 @@ export function getDefaultEditor(): string {
   }
 
   return "nano";
+}
+
+export function isDefaultEditorSet(): boolean {
+  let editor = gpExecSync({ command: `echo \${GIT_EDITOR}` })
+      .toString()
+      .trim();
+  if (!editor.length) {
+    editor = gpExecSync({command: `echo \${EDITOR}`}).toString().trim();
+  }
+  return editor.length !== 0;
+}
+
+export async function setDefaultEditorPrompt(): Promise<void> {
+
+  if (!isDefaultEditorSet()) {
+    logInfo(
+        chalk.yellow(
+            "Your default editor environment variables are not set. Do you wish to set it? (Graphite will use `nano` as default.)"
+        )
+    );
+  }
+  const yesOrNo = await prompts(
+      {
+        type: "select",
+        name: "editorPrompt",
+        message: "Set default editor now?",
+        choices: [
+          { title: `Yes (will set $EDITOR env variable).`, value: "yes" },
+          {
+            title: `Skip (just use nano as my default editor)`,
+            value: "no", // Should find a way to remember this selection so we are not repeatedly prompting
+          },
+        ],
+      },
+      {
+        onCancel: () => {
+          throw new KilledError();
+        },
+      }
+  );
+
+  if (yesOrNo.editorPrompt === 'yes') {
+    const editor = await prompts(
+        {
+          type: "text",
+          name: "editor",
+          message: "Enter your choice of editor (eg: vim, nano, emacs etc)", // Should this be a selection?
+          initial: 'nano',
+        },
+        {
+          onCancel: () => {
+            throw new KilledError();
+          },
+        }
+    );
+
+    gpExecSync({command: `export EDITOR=${editor}`}); // Will it require permissions? Will it persist or does it need to be added to .zsh?
+  }
 }
