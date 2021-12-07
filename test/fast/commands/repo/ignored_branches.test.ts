7a5914f51a13858f5157e4e834ae28595f29e1d4
Neha Srivastava <6138253+neha-sri@users.noreply.github.com>
2021-12-07 15:21:08 -0500

    fix(ignored): add test (#461)
    * fix(repo test): error in library to be fixed
    
    * fix(ignore): outline of changes needed
    
    * fix(ignored_branches): allow wildcard updates
    
    * fix(ignore): append if ignore branches is empty
    
    * fix(ignore): print matching branchesf for input pattern
    
    * fix(ignore): use branchIsIgnored instead of matching on name
    
    * fix(ignore): fix bug and replace unnecessary function
    
    * fix(ignore): remove TODOs + fix bug
    
    * fix(ignore): fix tests
    
    * fix(ignored): remove test)
    
    * fix(ignore_branches): better reg matching
    
    * fix(ignored-branches): use micromatch + nits
    
    * fix(ignored): add test
    
    * fix(ignore): split repo test to explicitly check ignore branches

--- test/fast/commands/repo/ignored_branches.test.ts
+++ test/fast/commands/repo/ignored_branches.test.ts
@@ -4,37 +4,35 @@ import { TrailingProdScene } from '../../../lib/scenes';
 import { configureTest } from '../../../lib/utils';
 
 for (const scene of [new TrailingProdScene()]) {
-  describe(`(${scene}): repo init`, function () {
+  describe(`(${scene}): repo ignored-branches`, function () {
     configureTest(this, scene);
 
-    it('Can run repo init', () => {
+    it('Can run add ignored-branches', () => {
       const repoConfigPath = `${scene.repo.dir}/.git/.graphite_repo_config`;
       fs.removeSync(repoConfigPath);
       scene.repo.execCliCommand('repo init --trunk main');
+      const branchToAdd = 'prod';
+      scene.repo.execCliCommand(`repo ignored-branches --add ${branchToAdd}`);
       const savedConfig = JSON.parse(
         fs.readFileSync(repoConfigPath).toString()
       );
-      expect(savedConfig['trunk']).to.eq('main');
+      expect(savedConfig['ignoreBranches'][0]).to.eq(branchToAdd);
     });
 
-    it('Can ignore branches at init', () => {
+    it('Can get ignored-branches', () => {
       const repoConfigPath = `${scene.repo.dir}/.git/.graphite_repo_config`;
       fs.removeSync(repoConfigPath);
-      const branchToIgnore = 'prod';
+      const branchToAdd = 'prod';
       scene.repo.execCliCommand(
-        `repo init --trunk main --ignore-branches ${branchToIgnore}`
+        `repo init --trunk main --ignore-branches ${branchToAdd}`
       );
+      expect(
+        scene.repo.execCliCommandAndGetOutput(`repo ignored-branches`)
+      ).to.contain(branchToAdd);
       const savedConfig = JSON.parse(
         fs.readFileSync(repoConfigPath).toString()
       );
-      expect(savedConfig['trunk']).to.eq('main');
-      expect(savedConfig['ignoreBranches'][0]).to.eq(branchToIgnore);
-    });
-
-    it('Cannot set an invalid trunk', () => {
-      expect(() =>
-        scene.repo.execCliCommand('repo init --trunk random')
-      ).to.throw(Error);
+      expect(savedConfig['ignoreBranches'][0]).to.eq(branchToAdd);
     });
   });
 }
