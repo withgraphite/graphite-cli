3adb346c666e9ae67da85d54ce7805ddab7579a3
Greg Foster <gregorymfoster@gmail.com>
2022-02-10 15:39:39 -0500

    style(edit): adjust edit file comments

--- src/actions/edit/create_stack_edit_file.ts
+++ src/actions/edit/create_stack_edit_file.ts
@@ -5,10 +5,24 @@ import Stack from '../../wrapper-classes/stack';
 
 const FILE_NAME = 'graphite_stack_edit';
 const COLUMN_SPACING = ' '.repeat(5);
-const FILE_HEADER = [`# Op`, `Branch`].join(COLUMN_SPACING);
-const FILE_DIVIDER = '# ' + `-`.repeat(20);
-const FILE_FOOTER =
-  '# p, pick = stack branch upon the branch from the previous line';
+const FILE_FOOTER = [
+  '#',
+  '# Operations:',
+  "# p, pick = stack onto the proceeding line's branch",
+];
+
+/* Example file:
+
+pick    gf--02-09-second
+pick    gf--02-09-first
+
+# pick  main (Bottom of stack)
+#
+# Operations:
+# pick <branch_name> stack branch upon the branch from the previous line
+
+*/
+
 export function createStackEditFile(opts: {
   stack: Stack;
   tmpDir: string;
@@ -20,12 +34,9 @@ export function createStackEditFile(opts: {
     .filter((n) => n !== trunkName);
   branchNames.reverse(); // show the trunk at the bottom of the list to better match "upstack" and "downstack"
   const fileContents = [
-    FILE_HEADER,
-    FILE_DIVIDER,
     ...branchNames.map((b) => `pick${COLUMN_SPACING}${b}`),
-    `# pick   ${trunkName} (shown here for orientation)`,
-    FILE_DIVIDER,
-    FILE_FOOTER,
+    `\n# pick   ${trunkName} (trunk, shown for orientation)`,
+    ...FILE_FOOTER,
   ].join('\n');
 
   const filePath = path.join(opts.tmpDir, FILE_NAME);
