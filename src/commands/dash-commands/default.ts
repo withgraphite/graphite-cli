a9a3e78454ff83ee5dcc47a9eaafb27dffa630b9
Greg Foster <gregorymfoster@gmail.com>
2022-02-14 14:22:07 -0500

    feat(new_command): open dash

---
+++ src/commands/dash-commands/default.ts
@@ -0,0 +1,20 @@
+import open from 'open';
+import yargs from 'yargs';
+import { profile } from '../../lib/telemetry';
+
+const args = {} as const;
+
+export const command = '*';
+export const description = 'Opens your Graphite dashboard in the web.';
+export const builder = args;
+export const canonical = 'dash';
+
+type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
+
+const DASHBOARD_URL = 'https://app.graphite.dev/';
+
+export const handler = async (argv: argsT): Promise<void> => {
+  return profile(argv, canonical, async () => {
+    void open(DASHBOARD_URL);
+  });
+};
