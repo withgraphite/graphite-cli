064ec3090ce88f09bc1ef3364da2f507fe863e15
Greg Foster <greg@graphite.dev>
2022-02-15 22:11:05 +0000

    Automated release: v0.17.11644963065-nightly

---
+++ dist/src/actions/clean_branches.d.ts
@@ -0,0 +1,10 @@
+import { MergeConflictCallstackT, TDeleteBranchesStackFrame } from '../lib/config/merge_conflict_callstack_config';
+/**
+ * This method is assumed to be idempotent -- if a merge conflict interrupts
+ * execution of this method, we simply restart the method upon running `gt
+ * continue`.
+ */
+export declare function deleteMergedBranches(opts: {
+    frame: TDeleteBranchesStackFrame;
+    parent: MergeConflictCallstackT;
+}): Promise<void>;
---
+++ dist/src/actions/edit/apply_stack_edit_pick.d.ts
@@ -0,0 +1,2 @@
+import { TStackEdit, TStackEditPick } from './stack_edits';
+export declare function applyStackEditPick(stackEdit: TStackEditPick, remainingEdits: TStackEdit[]): Promise<void>;
