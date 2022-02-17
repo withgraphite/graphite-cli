c36c157f1a4c02267d024305a28773e1bf1c485a
Greg Foster <greg@graphite.dev>
2022-02-17 22:11:19 +0000

    Automated release: v0.17.21645135878-nightly

---
+++ dist/src/actions/sync.d.ts
@@ -0,0 +1,10 @@
+import { TRepoSyncStackFrame } from '../lib/config/merge_conflict_callstack_config';
+export declare function syncAction(opts: {
+    pull: boolean;
+    force: boolean;
+    delete: boolean;
+    showDeleteProgress: boolean;
+    resubmit: boolean;
+    fixDanglingBranches: boolean;
+}): Promise<void>;
+export declare function repoSyncDeleteMergedBranchesContinuation(frame: TRepoSyncStackFrame): Promise<void>;
