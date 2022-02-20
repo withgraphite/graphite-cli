59a83a402f9eac461bef2ffeacf2ba77a0da2802
Greg Foster <greg@graphite.dev>
2022-02-20 22:10:17 +0000

    Automated release: v0.17.21645395017-nightly

---
+++ dist/src/actions/edit/stack_edits.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isValidStackEditType = void 0;
+function isValidStackEditType(type) {
+    if (type === 'pick') {
+        return true;
+    }
+    return false;
+}
+exports.isValidStackEditType = isValidStackEditType;
+//# sourceMappingURL=stack_edits.js.map
