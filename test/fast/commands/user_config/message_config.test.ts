3d409e2499c10c8c6b336566656907ab6c57c555
Greg Foster <gregorymfoster@gmail.com>
2022-02-25 14:02:03 -0500

    refactor(message_config): move to context

--- test/fast/commands/user_config/message_config.test.ts
+++ test/fast/commands/user_config/message_config.test.ts
@@ -1,18 +1,19 @@
 import { expect } from 'chai';
 import fs from 'fs-extra';
-import { messageConfigFactory } from '../../../../src/lib/config/message_config';
+import { initContext } from '../../../../src/lib/context/context';
 import { BasicScene } from '../../../lib/scenes';
 import { configureTest } from '../../../lib/utils';
+import { messageConfigFactory } from './../../../../src/lib/config/message_config';
 
 for (const scene of [new BasicScene()]) {
   describe(`(${scene}): upgrade message`, function () {
     configureTest(this, scene);
-    const messageConfig = messageConfigFactory.load();
+    const context = initContext();
 
     it('Sanity check - can read previously written message config', () => {
       const contents = 'Hello world!';
       const cliVersion = '0.0.0';
-      messageConfig.update(
+      context.messageConfig.update(
         (data) =>
           (data.message = {
             contents: contents,
@@ -20,27 +21,28 @@ for (const scene of [new BasicScene()]) {
           })
       );
 
-      const writtenContents = messageConfig.data.message?.contents;
-      const wirttenCLIVersion = messageConfig.data.message?.cliVersion;
+      const writtenMessageConfig = messageConfigFactory.load();
+      const writtenContents = writtenMessageConfig.data.message?.contents;
+      const wirttenCLIVersion = writtenMessageConfig.data.message?.cliVersion;
       expect(writtenContents === contents).to.be.true;
       expect(wirttenCLIVersion === cliVersion).to.be.true;
     });
 
     it('If no message, removes message config file', () => {
-      messageConfig.update((data) => (data.message = undefined));
-      expect(fs.existsSync(messageConfig.path)).to.be.false;
+      context.messageConfig.update((d) => (d.message = undefined));
+      expect(fs.existsSync(context.messageConfig.path)).to.be.false;
 
       // can handle removing the file "twice"
-      messageConfig.update((data) => (data.message = undefined));
-      expect(fs.existsSync(messageConfig.path)).to.be.false;
+      context.messageConfig.update((d) => (d.message = undefined));
+      expect(fs.existsSync(context.messageConfig.path)).to.be.false;
     });
 
     after(() => {
       // Make sure we clean up any temporary contents we wrote to the file.
       // Unlike the auth token, we don't need to worry about re-creating it
       // since the next run of the CLI will just re-fetch the upgrade prompt.
-      if (fs.existsSync(messageConfig.path)) {
-        fs.unlinkSync(messageConfig.path);
+      if (fs.existsSync(context.messageConfig.path)) {
+        fs.unlinkSync(context.messageConfig.path);
       }
     });
   });
