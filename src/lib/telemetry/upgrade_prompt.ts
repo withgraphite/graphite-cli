0820aef2283d78d48e44414eb829291c4b17cefe
Greg Foster <gregorymfoster@users.noreply.github.com>
2022-02-27 16:35:47 -0500

    refactor(message_config): move to retype (#565)
    **Context:**
    
    
    
    **Changes In This Pull Request:**
    
    
    
    **Test Plan:**
    :

--- src/lib/telemetry/upgrade_prompt.ts
+++ src/lib/telemetry/upgrade_prompt.ts
@@ -5,18 +5,18 @@ import cp from 'child_process';
 import { getUserEmail, SHOULD_REPORT_TELEMETRY } from '.';
 import { version } from '../../../package.json';
 import { API_SERVER } from '../api';
-import { messageConfig } from '../config';
+import { messageConfig } from '../config/message_config';
 import { TContext } from '../context/context';
 import { logMessageFromGraphite } from '../utils';
 
 function printAndClearOldMessage(): void {
-  const oldMessage = messageConfig.getMessage();
+  const oldMessage = messageConfig.data.message;
   // "Since we fetch the message asynchronously and display it when the user runs their next Graphite command,
   // double-check before showing the message if the CLI is still an old version
   // (i.e. the user hasn't updated the CLI in the meantime)."
   if (oldMessage && version == oldMessage.cliVersion) {
     logMessageFromGraphite(oldMessage.contents);
-    messageConfig.setMessage(undefined);
+    messageConfig.update((data) => (data.message = undefined));
   }
 }
 export function fetchUpgradePromptInBackground(context: TContext): void {
@@ -49,12 +49,15 @@ async function fetchUpgradePrompt(): Promise<void> {
 
     if (response._response.status == 200) {
       if (response.prompt) {
-        messageConfig.setMessage({
-          contents: response.prompt.message,
-          cliVersion: version,
-        });
+        messageConfig.update(
+          (data) =>
+            (data.message = {
+              contents: response.prompt!.message,
+              cliVersion: version,
+            })
+        );
       } else {
-        messageConfig.setMessage(undefined);
+        messageConfig.update((data) => (data.message = undefined));
       }
     }
   } catch (err) {
