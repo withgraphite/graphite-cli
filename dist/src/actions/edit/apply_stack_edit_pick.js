c0effa2e260c83848f84218a35b3fb0d72c96237
Greg Foster <greg@graphite.dev>
2022-03-18 22:09:34 +0000

    Automated release: v0.17.41647641374-nightly

---
+++ dist/src/actions/edit/apply_stack_edit_pick.js
@@ -0,0 +1,33 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.applyStackEditPick = void 0;
+const utils_1 = require("../../lib/utils");
+const branch_1 = require("../../wrapper-classes/branch");
+const stack_onto_1 = require("../onto/stack_onto");
+function applyStackEditPick(stackEdit, remainingEdits, context) {
+    return __awaiter(this, void 0, void 0, function* () {
+        utils_1.checkoutBranch(stackEdit.branchName);
+        yield stack_onto_1.stackOnto({
+            currentBranch: new branch_1.Branch(stackEdit.branchName),
+            onto: stackEdit.onto,
+            mergeConflictCallstack: [
+                {
+                    op: 'STACK_EDIT_CONTINUATION',
+                    currentBranchName: stackEdit.branchName,
+                    remainingEdits: remainingEdits,
+                },
+            ],
+        }, context);
+    });
+}
+exports.applyStackEditPick = applyStackEditPick;
+//# sourceMappingURL=apply_stack_edit_pick.js.map
---
+++ dist/src/lib/telemetry/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.registerSigintHandler = exports.postTelemetryInBackground = exports.fetchUpgradePromptInBackground = exports.SHOULD_REPORT_TELEMETRY = exports.getUserEmail = exports.profile = exports.tracer = void 0;
+const context_1 = require("./context");
+Object.defineProperty(exports, "getUserEmail", { enumerable: true, get: function () { return context_1.getUserEmail; } });
+const post_traces_1 = require("./post_traces");
+Object.defineProperty(exports, "postTelemetryInBackground", { enumerable: true, get: function () { return post_traces_1.postTelemetryInBackground; } });
+const profile_1 = require("./profile");
+Object.defineProperty(exports, "profile", { enumerable: true, get: function () { return profile_1.profile; } });
+const sigint_handler_1 = require("./sigint_handler");
+Object.defineProperty(exports, "registerSigintHandler", { enumerable: true, get: function () { return sigint_handler_1.registerSigintHandler; } });
+const tracer_1 = require("./tracer");
+Object.defineProperty(exports, "tracer", { enumerable: true, get: function () { return tracer_1.globalTracer; } });
+const upgrade_prompt_1 = require("./upgrade_prompt");
+Object.defineProperty(exports, "fetchUpgradePromptInBackground", { enumerable: true, get: function () { return upgrade_prompt_1.fetchUpgradePromptInBackground; } });
+const SHOULD_REPORT_TELEMETRY = process.env.NODE_ENV != 'development';
+exports.SHOULD_REPORT_TELEMETRY = SHOULD_REPORT_TELEMETRY;
+//# sourceMappingURL=index.js.map
