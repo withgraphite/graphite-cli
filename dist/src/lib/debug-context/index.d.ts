88b8434ad4509ed08f16d72c06621631ebe26fa3
Greg Foster <greg@graphite.dev>
2022-03-19 22:09:19 +0000

    Automated release: v0.17.41647727759-nightly

---
+++ dist/src/lib/debug-context/index.d.ts
@@ -0,0 +1,3 @@
+import { TContext } from './../context/context';
+export declare function captureState(context: TContext): string;
+export declare function recreateState(stateJson: string, context: TContext): string;
