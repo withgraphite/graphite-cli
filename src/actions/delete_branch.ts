c2328617924fc2868ec619a0a408d64adf829de7
Jacob Gold <jacob@graphite.dev>
2022-03-23 12:57:48 -0400

    feat: add hint about -D for gt bdl (#592)
    **Context:**
    
    
    
    **Changes In This Pull Request:**
    
    
    
    **Test Plan:**
    :

--- src/actions/delete_branch.ts
+++ src/actions/delete_branch.ts
@@ -1,16 +1,24 @@
+import { TContext } from 'src/lib/context/context';
 import { ExitFailedError } from '../lib/errors';
-import { gpExecSync } from '../lib/utils';
+import { gpExecSync, logTip } from '../lib/utils';
 import { MetadataRef } from '../wrapper-classes';
 
-export function deleteBranchAction(args: {
-  branchName: string;
-  force: boolean;
-}): void {
+export function deleteBranchAction(
+  args: {
+    branchName: string;
+    force: boolean;
+  },
+  context?: TContext
+): void {
   const meta = new MetadataRef(args.branchName);
 
   // No need for a try-catch here; this already silently does nothing if the
   // metadata does not exist.
   meta.delete();
+
+  if (context && !args.force) {
+    logTip(`You can force branch deletion with -D`, context);
+  }
 
   gpExecSync(
     {
