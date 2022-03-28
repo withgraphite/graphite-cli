6fcc8c6461bbfe1bd82ca079b5cc34f27937bc47
Greg Foster <greg@graphite.dev>
2022-03-28 22:09:25 +0000

    Automated release: v0.17.51648505365-nightly

---
+++ dist/src/lib/utils/single_commit.d.ts
@@ -0,0 +1,4 @@
+import { Commit } from '../../wrapper-classes';
+import { Branch } from '../../wrapper-classes/branch';
+import { TContext } from '../context/context';
+export declare function getSingleCommitOnBranch(branch: Branch, context: TContext): Commit | null;
