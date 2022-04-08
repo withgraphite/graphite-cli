d164167be419c9ec659865502aa8f4f2fd0a4195
Jacob Gold <jacob@graphite.dev>
2022-04-08 11:44:50 -0400

    fix: completions (to be included in 0.17.7) (#605)
    **Context:**
    
    
    
    **Changes In This Pull Request:**
    
    
    
    **Test Plan:**
    :

--- src/commands/autocomplete.ts
+++ src/commands/autocomplete.ts
@@ -32,59 +32,25 @@ yargs.completion('completion', (current, argv) => {
  * gt log => null
  */
 function getBranchArg(current: string, argv: Arguments): string | null {
-  const currentCommand = argv['_'].join(' ');
-
-  // gt branch checkout --branch <branch_name>
   if (
-    (currentCommand.includes('branch checkout') ||
-      currentCommand.includes('b checkout')) &&
-    'branch' in argv
-  ) {
-    // Because --branch is an option on the overall command, we need to check
-    // the value of current to make sure that the branch argument is the
-    // current argument being entered by the user.
-    if (current === '--branch') {
-      return '';
-    } else if (current === argv['branch']) {
-      return current;
-    }
-  }
-
-  // gt bco
-  // Check membership in argv to ensure that "bco" is its own entry (and not
-  // a substring of another command). Since we're dealing with a positional,
-  // we also want to make sure that the current argument is the positional
-  // (position 3).
-  if (
-    argv['_'].includes('bco') &&
-    argv['_'].length <= 3 &&
+    // gt bco
+    // Check membership in argv to ensure that "bco" is its own entry (and not
+    // a substring of another command). Since we're dealing with a positional,
+    // we also want to make sure that the current argument is the positional
+    // (position 3).
+    ((argv['_'].length <= 3 && argv['_'][1] === 'bco') ||
+      // gt branch checkout / b co (and permutations)
+      // same as above, but one position further
+      (argv['_'].length <= 4 &&
+        (argv['_'][1] === 'b' || argv['_'][1] === 'branch') &&
+        (argv['_'][2] === 'co' || argv['_'][2] === 'checkout')) ||
+      // gt upstack onto / us onto
+      ((argv['_'][1] === 'upstack' || argv['_'][1] === 'us') &&
+        argv['_'][2] === 'onto')) &&
     typeof current === 'string'
   ) {
+    // this handles both with and without --branch because it's the only string arg
     return current;
-  }
-
-  // gt b co
-  if (
-    currentCommand.includes('b co') &&
-    argv['_'].includes('b') &&
-    argv['_'].includes('co') &&
-    argv['_'].length <= 4 &&
-    typeof current === 'string'
-  ) {
-    return current;
-  }
-
-  // gt upstack onto <branch_name>
-  if (
-    currentCommand.includes('upstack onto') ||
-    currentCommand.includes('us onto')
-  ) {
-    // Again, since we're detailing with a positional, we want to make sure
-    // that the current argument being entered is in the desired position,
-    // i.e. position 4.
-    if (argv['_'].length <= 4 && typeof current === 'string') {
-      return current;
-    }
   }
 
   return null;
