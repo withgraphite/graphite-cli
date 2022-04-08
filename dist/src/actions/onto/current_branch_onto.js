d8500e638d6dd06ae50b7c585c180c1f523b67d5
Jacob Gold <jacob@graphite.dev>
2022-04-08 11:52:38 -0400

    Automated release: v0.17.7

---
+++ dist/src/actions/onto/current_branch_onto.js
@@ -0,0 +1,32 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.currentBranchOntoAction = void 0;
+const errors_1 = require("../../lib/errors");
+const preconditions_1 = require("../../lib/preconditions");
+const utils_1 = require("../../lib/utils");
+const stack_onto_1 = require("./stack_onto");
+function currentBranchOntoAction(args, context) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (utils_1.trackedUncommittedChanges()) {
+            throw new errors_1.PreconditionsFailedError('Cannot fix with uncommitted changes');
+        }
+        const originalBranch = preconditions_1.currentBranchPrecondition(context);
+        yield stack_onto_1.stackOnto({
+            currentBranch: originalBranch,
+            onto: args.onto,
+            mergeConflictCallstack: args.mergeConflictCallstack,
+        }, context);
+        utils_1.checkoutBranch(originalBranch.name);
+    });
+}
+exports.currentBranchOntoAction = currentBranchOntoAction;
+//# sourceMappingURL=current_branch_onto.js.map
---
+++ dist/src/commands/autocomplete.js
@@ -0,0 +1,55 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const yargs_1 = __importDefault(require("yargs"));
+const context_1 = require("../lib/context/context");
+const branch_1 = require("../wrapper-classes/branch");
+yargs_1.default.completion('completion', (current, argv) => {
+    const context = context_1.initContext();
+    const branchArg = getBranchArg(current, argv);
+    if (branchArg === null) {
+        return;
+    }
+    return branch_1.Branch.allBranchesWithFilter({
+        filter: (b) => b.name.startsWith(branchArg),
+    }, context)
+        .map((b) => b.name)
+        .sort();
+});
+/**
+ * If the user is entering a branch argument, returns the current entered
+ * value. Else, returns null.
+ *
+ * e.g.
+ *
+ * gt branch checkout --branch ny--xyz => 'ny--xyz'
+ * gt branch checkout --branch => ''
+ *
+ * gt repo sync => null
+ * gt log => null
+ */
+function getBranchArg(current, argv) {
+    if (
+    // gt bco
+    // Check membership in argv to ensure that "bco" is its own entry (and not
+    // a substring of another command). Since we're dealing with a positional,
+    // we also want to make sure that the current argument is the positional
+    // (position 3).
+    ((argv['_'].length <= 3 && argv['_'][1] === 'bco') ||
+        // gt branch checkout / b co (and permutations)
+        // same as above, but one position further
+        (argv['_'].length <= 4 &&
+            (argv['_'][1] === 'b' || argv['_'][1] === 'branch') &&
+            (argv['_'][2] === 'co' || argv['_'][2] === 'checkout')) ||
+        // gt upstack onto / us onto
+        ((argv['_'][1] === 'upstack' || argv['_'][1] === 'us') &&
+            argv['_'][2] === 'onto')) &&
+        typeof current === 'string') {
+        // this handles both with and without --branch because it's the only string arg
+        return current;
+    }
+    return null;
+}
+//# sourceMappingURL=autocomplete.js.map
