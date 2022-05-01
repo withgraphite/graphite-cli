a7f23c8b53647a0a76ad757a40a5a949d70aad48
Jacob Gold <jacob@graphite.dev>
2022-05-01 13:36:54 -0400

    feat: gt branch edit

---
+++ src/actions/edit_branch.ts
@@ -0,0 +1,45 @@
+import { TContext } from '../lib/context/context';
+import {
+  ExitFailedError,
+  PreconditionsFailedError,
+  RebaseConflictError,
+} from '../lib/errors';
+import { currentBranchPrecondition } from '../lib/preconditions';
+import { gpExecSync, rebaseInProgress } from '../lib/utils';
+import { rebaseUpstack } from './fix';
+
+export async function editBranchAction(context: TContext): Promise<void> {
+  const currentBranch = currentBranchPrecondition(context);
+
+  const baseRev = currentBranch.getParentBranchSha();
+  if (!baseRev) {
+    throw new PreconditionsFailedError(
+      `Graphite does not have a base revision for this branch; it might have been created with an older version of Graphite.  Please run a 'fix' or 'validate' command in order to backfill this information.`
+    );
+  }
+
+  gpExecSync(
+    {
+      command: `git rebase -i ${baseRev}`,
+      options: { stdio: 'inherit' },
+    },
+    (err) => {
+      if (rebaseInProgress()) {
+        throw new RebaseConflictError(
+          `Interactive rebase in progress.  After resolving merge conflicts, run 'gt continue'`,
+          [
+            {
+              op: 'STACK_FIX' as const,
+              sourceBranchName: currentBranch.name,
+            },
+          ],
+          context
+        );
+      } else {
+        throw new ExitFailedError(`Interactive rebase failed.`, err);
+      }
+    }
+  );
+
+  await rebaseUpstack(context);
+}
---
+++ src/commands/branch-commands/edit.ts
@@ -0,0 +1,18 @@
+import yargs from 'yargs';
+import { editBranchAction } from '../../actions/edit_branch';
+import { profile } from '../../lib/telemetry';
+
+const args = {} as const;
+type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
+
+export const aliases = ['e'];
+export const command = 'edit';
+export const canonical = 'branch edit';
+export const description =
+  'Run an interactive rebase on the entire current branch and fix upstack branches.';
+export const builder = args;
+export const handler = async (argv: argsT): Promise<void> => {
+  return profile(argv, canonical, async (context) => {
+    await editBranchAction(context);
+  });
+};
