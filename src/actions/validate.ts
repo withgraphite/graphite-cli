ed56999726149555abc5efa1ef7a16245f8a151f
Jacob Gold <jacob@graphite.dev>
2022-05-01 23:46:41 -0400

    fix: use memoization for validation

--- src/actions/validate.ts
+++ src/actions/validate.ts
@@ -2,27 +2,21 @@ import { TContext } from '../lib/context/context';
 import { ValidationFailedError } from '../lib/errors';
 import { currentBranchPrecondition } from '../lib/preconditions';
 import { logInfo } from '../lib/utils';
-import { GitStackBuilder, MetaStackBuilder, Stack } from '../wrapper-classes';
+import { GitStackBuilder, MetaStackBuilder } from '../wrapper-classes';
 import { Branch } from '../wrapper-classes/branch';
 import { TScope } from './scope';
 
 export function validate(scope: TScope, context: TContext): Branch[] {
   const currentBranch = currentBranchPrecondition(context);
-  const gitStack = new GitStackBuilder().getStack(
+  const gitStack = new GitStackBuilder({ useMemoizedResults: true }).getStack(
     { currentBranch, scope },
     context
   );
-  const metaStack = new MetaStackBuilder().getStack(
+  const metaStack = new MetaStackBuilder({ useMemoizedResults: true }).getStack(
     { currentBranch, scope },
     context
   );
-  compareStacks(metaStack, gitStack);
 
-  logInfo(`Current stack is valid`);
-  return metaStack.branches().filter((b) => !b.isTrunk(context));
-}
-
-function compareStacks(metaStack: Stack, gitStack: Stack): void {
   if (!metaStack.equals(gitStack)) {
     throw new ValidationFailedError(
       [
@@ -34,4 +28,7 @@ function compareStacks(metaStack: Stack, gitStack: Stack): void {
       ].join('\n')
     );
   }
+
+  logInfo(`Current stack is valid`);
+  return metaStack.branches().filter((b) => !b.isTrunk(context));
 }
