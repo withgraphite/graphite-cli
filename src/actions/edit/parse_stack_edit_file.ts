2bfd4a7f8a69283c4a4e2ea5add83e19563f2b13
Jacob Gold <jacob@graphite.dev>
2022-05-03 01:31:31 -0400

    feat: improve stack edit parsing

--- src/actions/edit/parse_stack_edit_file.ts
+++ src/actions/edit/parse_stack_edit_file.ts
@@ -2,41 +2,49 @@ import fs from 'fs-extra';
 import { TContext } from '../../lib/context/context';
 import { ExitFailedError } from '../../lib/errors';
 import { getTrunk } from '../../lib/utils';
-import {
-  isValidStackEditType,
-  TStackEdit,
-  TStackEditType,
-} from './stack_edits';
+import { getStackEditType, TStackEdit, TStackEditType } from './stack_edits';
+
+// https://regex101.com/r/j0ohLA/1
+const LINE_REGEX = /([a-z]*)[ ,]+(.*)/;
 
 export function parseEditFile(
   opts: { filePath: string },
   context: TContext
 ): TStackEdit[] {
-  const fileContents = fs.readFileSync(opts.filePath).toString();
-  const parsedEdit = fileContents
+  const parsedEdit = fs
+    .readFileSync(opts.filePath)
+    .toString()
     .split('\n')
-    .map((line) => line.split(/[ ,]+/))
-    .filter((lineParts) => {
-      // Each line, we want to split into two parts (edit type and branch name)
-      return lineParts.length === 2 && isValidStackEditType(lineParts[0]);
-    })
-    .map((lineParts) => {
-      return { type: lineParts[0] as TStackEditType, branchName: lineParts[1] };
-    });
-
-  parsedEdit.reverse();
-
-  if (parsedEdit.map((e) => e.branchName).includes(getTrunk(context).name)) {
-    throw new ExitFailedError(`Cannot perform edits on trunk branch`);
-  }
+    .reverse()
+    .map((line) =>
+      line.substring(0, line.includes('#') ? line.indexOf('#') : line.length)
+    )
+    .filter((line) => line.length > 0)
+    .map(parseLine);
 
   return parsedEdit.map((parsedStackEdit, index) => {
     // Assume all edits are PICKs for now
+    if (parsedStackEdit.rest === getTrunk(context).name) {
+      throw new ExitFailedError(`Cannot perform edits on trunk branch`);
+    }
     return {
       type: parsedStackEdit.type,
-      branchName: parsedStackEdit.branchName,
-      onto:
-        index === 0 ? getTrunk(context).name : parsedEdit[index - 1].branchName,
+      branchName: parsedStackEdit.rest,
+      onto: index === 0 ? getTrunk(context).name : parsedEdit[index - 1].rest,
     };
   });
+}
+
+function parseLine(line: string): { type: TStackEditType; rest: string } {
+  const match = line.match(LINE_REGEX);
+  if (!match) {
+    throw new ExitFailedError(`Invalid edit: ${line}`);
+  }
+
+  const type = getStackEditType(match[1]);
+  if (!type) {
+    throw new ExitFailedError(`Invalid edit: ${line}`);
+  }
+
+  return { type, rest: match[2] };
 }
