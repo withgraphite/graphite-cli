08a1d25b80e7210669e4f6ffc3fdde2c8964f757
Jacob Gold <jacob@graphite.dev>
2022-05-04 17:15:17 -0400

    refactor: use checkout_branch lib for create_branch action

--- src/lib/utils/checkout_branch.ts
+++ src/lib/utils/checkout_branch.ts
@@ -3,12 +3,18 @@ import { gpExecSync } from './exec_sync';
 
 export function checkoutBranch(
   branch: string,
-  opts?: { quiet?: boolean }
+  opts?: { quiet?: boolean; new?: boolean }
 ): void {
   gpExecSync(
-    { command: `git switch ${opts?.quiet ? '-q' : ''} "${branch}"` },
+    {
+      command: `git switch ${opts?.quiet ? '-q' : ''} ${
+        opts?.new ? '-c' : ''
+      }"${branch}"`,
+    },
     () => {
-      throw new ExitFailedError(`Failed to checkout branch (${branch})`);
+      throw new ExitFailedError(
+        `Failed to checkout ${opts?.new ? 'new ' : ''}branch (${branch})`
+      );
     }
   );
 }
