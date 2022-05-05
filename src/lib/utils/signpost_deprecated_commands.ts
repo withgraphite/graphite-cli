9f0e2fdd58b3c11f52aee04d026926601dd9872d
Jacob Gold <jacob@graphite.dev>
2022-05-05 01:44:56 -0400

    feat: kill stacks warning

--- src/lib/utils/signpost_deprecated_commands.ts
+++ src/lib/utils/signpost_deprecated_commands.ts
@@ -6,12 +6,6 @@ const BRANCH_WARNING = [
   'Thank you for bearing with us while we rapidly iterate!',
 ].join('\n');
 
-const STACKS_WARNING = [
-  'The command "stacks" has been deprecated.',
-  'Please use "log short" aka "ls" instead to see your stacks, or "branch checkout" aka "bco" for an interactive checkout.',
-  'Thank you for bearing with us while we rapidly iterate!',
-].join('\n');
-
 export function signpostDeprecatedCommands(command: string[]): void {
   if (command.length === 0) return;
   switch (command[0]) {
@@ -21,10 +15,5 @@ export function signpostDeprecatedCommands(command: string[]): void {
         logWarn(BRANCH_WARNING);
       }
       break;
-
-    case 'stacks':
-      logWarn(STACKS_WARNING);
-      // eslint-disable-next-line no-restricted-syntax
-      process.exit(1);
   }
 }
