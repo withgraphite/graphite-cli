4da1631fa315c7c2ec8265eedc403c2408ad6fbe
Jacob Gold <jacob@graphite.dev>
2022-05-10 16:53:55 -0400

    feat: get all metadata refs

--- src/lib/validation/validate.ts
+++ src/lib/validation/validate.ts
@@ -1,7 +1,27 @@
+import { MetadataRef } from '../../wrapper-classes/metadata_ref';
 import { TContext } from '../context';
 import { logInfo } from '../utils/splog';
 
+type TAllBranchRelations = Map<
+  string,
+  { parentName?: string; parentRevision?: string }
+>;
+
 export function validate(context: TContext): void {
+  const allBranchRelations: TAllBranchRelations = new Map(
+    MetadataRef.allMetadataRefs().map((ref) => {
+      const meta = ref.read();
+      return [
+        ref._branchName,
+        {
+          parentName: meta?.parentBranchName,
+          parentRevision: meta?.parentBranchRevision,
+        },
+      ];
+    })
+  );
   void context;
-  logInfo('validating');
+  allBranchRelations.forEach((value, key) => {
+    logInfo(`${key}: ${JSON.stringify(value)}`);
+  });
 }
