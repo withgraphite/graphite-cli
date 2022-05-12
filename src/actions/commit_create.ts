0dedb4bad9438e29020e39fd5950dd2947933e2d
Jacob Gold <jacob@graphite.dev>
2022-05-12 09:48:43 -0400

    chore(bump): 0.18.2 and a small temporary fix

--- src/actions/commit_create.ts
+++ src/actions/commit_create.ts
@@ -2,6 +2,7 @@ import { TContext } from '../lib/context';
 import { ensureSomeStagedChangesPrecondition } from '../lib/preconditions';
 import { addAll } from '../lib/utils/addAll';
 import { commit } from '../lib/utils/commit';
+import { Branch } from '../wrapper-classes/branch';
 import { rebaseUpstack } from './fix';
 
 export async function commitCreateAction(
@@ -16,6 +17,15 @@ export async function commitCreateAction(
   }
 
   ensureSomeStagedChangesPrecondition(context);
+
+  // TODO we will kill this once we cut over to relying on parentRevision for fix
+  // If we're checked out on a branch, we're going to perform a stack fix later.
+  // In order to allow the stack fix to cut out the old commit, we need to set
+  // the prev ref here.
+  const currentBranch = Branch.getCurrentBranch();
+  if (currentBranch !== null) {
+    currentBranch.savePrevRef();
+  }
 
   commit({ message: opts.message });
 
--- src/actions/edit_branch.ts
+++ src/actions/edit_branch.ts
@@ -19,6 +19,14 @@ export async function editBranchAction(context: TContext): Promise<void> {
     );
   }
 
+  // TODO we will kill this once we cut over to relying on parentRevision for fix
+  // If we're checked out on a branch, we're going to perform a stack fix later.
+  // In order to allow the stack fix to cut out the old commit, we need to set
+  // the prev ref here.
+  if (currentBranch !== null) {
+    currentBranch.savePrevRef();
+  }
+
   gpExecSync(
     {
       command: `git rebase -i ${baseRev}`,
