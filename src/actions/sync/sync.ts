6dedcece831b3d42ee06ffd0af70ab492e926842
Jacob Gold <jacob@graphite.dev>
2022-05-15 20:44:27 -0400

    feat: kill dangling branch step in sync

--- src/actions/sync/sync.ts
+++ src/actions/sync/sync.ts
@@ -10,7 +10,6 @@ import { syncPRInfoForBranches } from '../../lib/sync/pr_info';
 import { getTrunk } from '../../lib/utils/trunk';
 import { Branch } from '../../wrapper-classes/branch';
 import { cleanBranches as cleanBranches } from '../clean_branches';
-import { fixDanglingBranches } from '../fix_dangling_branches';
 import { mergeDownstack } from './merge_downstack';
 import { pull } from './pull';
 import { resubmitBranchesWithNewBases } from './resubmit_branches_with_new_bases';
@@ -21,7 +20,6 @@ export async function syncAction(
     delete: boolean;
     showDeleteProgress: boolean;
     resubmit: boolean;
-    fixDanglingBranches: boolean;
     downstackToSync?: string[];
   },
   context: TContext
@@ -47,15 +45,6 @@ export async function syncAction(
   }
 
   await syncPRInfoForBranches(Branch.allBranches(context), context);
-
-  // This needs to happen before we delete/resubmit so that we can potentially
-  // delete or resubmit on the dangling branches.
-  if (opts.fixDanglingBranches) {
-    await fixDanglingBranches(context, {
-      force: opts.force,
-      showSyncTip: true,
-    });
-  }
 
   const deleteMergedBranchesContinuation = {
     op: 'REPO_SYNC_CONTINUATION' as const,
@@ -97,22 +86,3 @@ export async function cleanBranchesContinuation(
     { quiet: true }
   );
 }
-
-/**
- *
- * Remove for now - users are reporting issues where this is incorrectly
- * deleting metadata for still-existing branches.
- *
- * https://graphite-community.slack.com/archives/C02DRNRA9RA/p1632897956089100
- * https://graphite-community.slack.com/archives/C02DRNRA9RA/p1634168133170500
- *
-function cleanDanglingMetadata(): void {
-  const allMetadataRefs = MetadataRef.allMetadataRefs();
-  allMetadataRefs.forEach((ref) => {
-    if (!branchExists(ref._branchName)) {
-      logDebug(`Deleting metadata for ${ref._branchName}`);
-      ref.delete();
-    }
-  });
-}
-*/
