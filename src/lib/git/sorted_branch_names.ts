cc08845287a3e29ccde0b4f16cdc65e42b5963fa
Jacob Gold <jacob@graphite.dev>
2022-05-27 19:40:05 -0400

    refactor: map to record

--- src/lib/git/sorted_branch_names.ts
+++ src/lib/git/sorted_branch_names.ts
@@ -8,17 +8,20 @@ export function sortedBranchNames(): string[] {
     .filter((branchName) => branchName.length > 0);
 }
 
-export function branchNamesAndRevisions(): Map<string, string> {
-  return new Map(
-    gpExecSync({
-      command: `git for-each-ref --format='%(refname:short):%(objectname)' refs/heads/`,
-    })
-      .split('\n')
-      .filter((line) => line.length > 0)
-      .map((line) => line.split(':'))
-      .filter(
-        (lineSplit): lineSplit is [string, string] =>
-          lineSplit.length === 2 && lineSplit.every((s) => s.length > 0)
-      )
-  );
+export function branchNamesAndRevisions(): Record<string, string> {
+  const branches: Record<string, string> = {};
+
+  gpExecSync({
+    command: `git for-each-ref --format='%(refname:short):%(objectname)' refs/heads/`,
+  })
+    .split('\n')
+    .filter((line) => line.length > 0)
+    .map((line) => line.split(':'))
+    .filter(
+      (lineSplit): lineSplit is [string, string] =>
+        lineSplit.length === 2 && lineSplit.every((s) => s.length > 0)
+    )
+    .forEach(([branchName, sha]) => (branches[branchName] = sha));
+
+  return branches;
 }
