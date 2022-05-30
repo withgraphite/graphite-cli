09da46e83df2a523cdc088e5dae4bdf04154fa3b
Jacob Gold <jacob@graphite.dev>
2022-05-30 00:50:01 -0400

    refactor: clean up pr info sync

--- src/lib/sync/pr_info.ts
+++ src/lib/sync/pr_info.ts
@@ -2,6 +2,7 @@ import graphiteCLIRoutes from '@withgraphite/graphite-cli-routes';
 import { request } from '@withgraphite/retyped-routes';
 import { API_SERVER } from '../api';
 import { TContext } from '../context';
+import { TBranchPRInfo } from '../engine/metadata_ref';
 
 export async function syncPRInfoForBranches(
   branchNames: string[],
@@ -12,47 +13,65 @@ export async function syncPRInfoForBranches(
     return;
   }
 
-  const repoName = context.repoConfig.getRepoName();
-  const repoOwner = context.repoConfig.getRepoOwner();
+  const branchesWithoutPrInfo = new Set<string>();
+  const existingPrInfo = new Map<
+    number,
+    TBranchPRInfo & { branchName: string }
+  >();
+
+  branchNames.forEach((branchName) => {
+    const prInfo = context.metaCache.getPrInfo(branchName);
+
+    if (prInfo?.number === undefined) {
+      branchesWithoutPrInfo.add(branchName);
+    } else {
+      existingPrInfo.set(prInfo.number, {
+        ...prInfo,
+        branchName: branchName,
+      });
+    }
+  });
 
   const response = await request.requestWithArgs(
     API_SERVER,
     graphiteCLIRoutes.pullRequestInfo,
     {
       authToken: authToken,
-      repoName: repoName,
-      repoOwner: repoOwner,
-      prNumbers: branchNames
-        .map((branch) => context.metaCache.getPrInfo(branch)?.number)
-        .filter((value): value is number => value !== undefined),
+      repoName: context.repoConfig.getRepoName(),
+      repoOwner: context.repoConfig.getRepoOwner(),
+      prNumbers: [...existingPrInfo.keys()],
       // For branches that are not already associated with a PR, fetch by branch name.
-      prHeadRefNames: branchNames.filter(
-        (branch) => !context.metaCache.getPrInfo(branch)?.number === undefined
-      ),
+      prHeadRefNames: [...branchesWithoutPrInfo],
     }
   );
 
-  if (response._response.status === 200) {
-    // Note that this currently does not play nicely if the user has a branch
-    // that is being merged into multiple other branches; we expect this to
-    // be a rare case and will develop it lazily.
-    response.prs
-      .filter(
-        (pr) =>
-          // If the PR is not open, don't newly associate it with a branch.
-          pr.state === 'OPEN' ||
-          context.metaCache.getPrInfo(pr.headRefName)?.number !== undefined
-      )
-      .forEach((pr) =>
-        context.metaCache.upsertPrInfo(pr.headRefName, {
-          number: pr.prNumber,
-          base: pr.baseRefName,
-          url: pr.url,
-          state: pr.state,
-          title: pr.title,
-          reviewDecision: pr.reviewDecision ?? undefined,
-          isDraft: pr.isDraft,
-        })
-      );
+  if (response._response.status !== 200) {
+    return;
   }
+
+  response.prs
+    .filter((pr) => {
+      const existingInfoForPr = existingPrInfo.get(pr.prNumber);
+
+      const shouldUpdateExistingBranch =
+        existingInfoForPr?.branchName === pr.headRefName;
+
+      const shouldAssociatePrWithBranch =
+        !existingInfoForPr &&
+        pr.state === 'OPEN' &&
+        branchesWithoutPrInfo.has(pr.headRefName);
+
+      return shouldAssociatePrWithBranch || shouldUpdateExistingBranch;
+    })
+    .forEach((pr) =>
+      context.metaCache.upsertPrInfo(pr.headRefName, {
+        number: pr.prNumber,
+        base: pr.baseRefName,
+        url: pr.url,
+        state: pr.state,
+        title: pr.title,
+        reviewDecision: pr.reviewDecision ?? undefined,
+        isDraft: pr.isDraft,
+      })
+    );
 }
