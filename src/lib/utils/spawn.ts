a48eedbdc6b95768e7d71a760f3f678fff23c648
Jacob Gold <jacob@graphite.dev>
2022-05-31 14:22:54 -0400

    fix: remove unnecessary /usr/bin/env (#900)
    Co-authored-by: Rosetta Roberts <rosettafroberts@gmail.com>

--- src/lib/utils/spawn.ts
+++ src/lib/utils/spawn.ts
@@ -2,7 +2,7 @@ import cp from 'child_process';
 
 // Spawns an async process that executes the specified file
 export function spawnDetached(filename: string, args: string[] = []): void {
-  cp.spawn('/usr/bin/env', ['node', filename, ...args], {
+  cp.spawn('node', [filename, ...args], {
     detached: true,
     stdio: 'ignore',
   }).unref();
