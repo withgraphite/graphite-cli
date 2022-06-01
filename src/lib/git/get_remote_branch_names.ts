4a44cb6eaaf20bbf43b57d3bfe0e41ef9a629b1a
Jacob Gold <jacob@graphite.dev>
2022-06-01 04:11:11 -0400

    feat: first pass at ds sync

--- src/lib/git/get_remote_branch_names.ts
+++
@@ -1,8 +0,0 @@
-import { TContext } from '../context';
-import { gpExecSyncAndSplitLines } from '../utils/exec_sync';
-
-export function getRemoteBranchNames(context: TContext): string[] {
-  return gpExecSyncAndSplitLines({
-    command: `git ls-remote -h ${context.repoConfig.getRemote()}`,
-  }).map((line) => line.split('refs/heads/')[1]);
-}
