20eb4741037a0e510f13c7a6e3d12c7d40a21f66
Jacob Gold <jacob@graphite.dev>
2022-05-31 20:39:19 -0400

    refactor: move repo sync to cache

--- src/actions/sync/sync.ts
+++ src/actions/sync/sync.ts
@@ -1,11 +1,11 @@
+import chalk from 'chalk';
 import { TContext } from '../../lib/context';
 import { SCOPE } from '../../lib/engine/scope_spec';
+import { ExitFailedError } from '../../lib/errors';
 import { uncommittedTrackedChangesPrecondition } from '../../lib/preconditions';
-import { cleanBranches as cleanBranches } from '../clean_branches';
+import { cleanBranches } from '../clean_branches';
 import { restackBranches } from '../restack';
 import { syncPrInfo } from '../sync_pr_info';
-import { mergeDownstack } from './merge_downstack';
-import { pull } from './pull';
 
 export async function syncAction(
   opts: {
@@ -19,17 +19,35 @@ export async function syncAction(
 ): Promise<void> {
   uncommittedTrackedChangesPrecondition();
   if (opts.pull) {
-    context.splog.logInfo(`Pulling in new changes...`);
+    context.splog.logInfo(
+      `Pulling ${chalk.cyan(context.metaCache.trunk)} from remote...`
+    );
     context.splog.logTip(
       `Disable this behavior at any point in the future with --no-pull`
     );
-    pull([context.metaCache.trunk].concat(opts.downstackToSync ?? []), context);
-    context.splog.logNewline();
+
+    try {
+      context.splog.logInfo(
+        context.metaCache.pullTrunk() === 'PULL_UNNEEDED'
+          ? `${chalk.green(context.metaCache.trunk)} is up to date.`
+          : `${chalk.green(
+              context.metaCache.trunk
+            )} fast-forwarded to ${chalk.gray(
+              context.metaCache.getRevision(context.metaCache.trunk)
+            )}.`
+      );
+      context.splog.logNewline();
+    } catch (err) {
+      throw new ExitFailedError(`Failed to pull trunk`, err);
+    }
   }
 
-  if (opts.downstackToSync) {
-    await mergeDownstack(opts.downstackToSync, context);
-  }
+  // TODO upstack PR will address this
+  // if (opts.downstackToSync) {
+  //     pull(opts.downstackToSync ?? []), context);
+
+  //   await mergeDownstack(opts.downstackToSync, context);
+  // }
 
   await syncPrInfo(context.metaCache.allBranchNames, context);
 
