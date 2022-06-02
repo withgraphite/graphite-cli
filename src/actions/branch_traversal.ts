88a738d86e1368e84c0a6ddd5c711678f3cf95da
Jacob Gold <jacob@graphite.dev>
2022-06-02 19:09:59 -0400

    feat: clean up traversal formatting

--- src/actions/branch_traversal.ts
+++ src/actions/branch_traversal.ts
@@ -3,92 +3,6 @@ import prompts from 'prompts';
 import { TContext } from '../lib/context';
 import { ExitFailedError, KilledError } from '../lib/errors';
 
-// eslint-disable-next-line max-params
-function traverseDownward(
-  currentBranchName: string,
-  context: TContext,
-  stepsRemaining: number | 'bottom' = 'bottom',
-  indent = 0
-): string {
-  context.splog.logInfo(
-    `${'  '.repeat(indent)}↳ ${chalk.blue(currentBranchName)}`
-  );
-  if (stepsRemaining === 0 || context.metaCache.isTrunk(currentBranchName)) {
-    return currentBranchName;
-  }
-  const parentBranchName =
-    context.metaCache.getParentPrecondition(currentBranchName);
-  if (
-    stepsRemaining === 'bottom' &&
-    context.metaCache.isTrunk(parentBranchName)
-  ) {
-    return currentBranchName;
-  }
-  return traverseDownward(
-    parentBranchName,
-    context,
-    stepsRemaining === 'bottom' ? 'bottom' : stepsRemaining - 1,
-    indent + 1
-  );
-}
-
-// eslint-disable-next-line max-params
-async function traverseUpward(
-  currentBranchName: string,
-  context: TContext,
-  stepsRemaining: number | 'top' = 'top',
-  indent = 0
-): Promise<string> {
-  context.splog.logInfo(
-    `${'  '.repeat(indent)}↳ ${chalk.blue(currentBranchName)}`
-  );
-  if (stepsRemaining === 0) {
-    return currentBranchName;
-  }
-  const children = context.metaCache.getChildren(currentBranchName);
-  if (children.length === 0) {
-    return currentBranchName;
-  }
-  const childBranchName =
-    children.length === 1
-      ? children[0]
-      : await handleMultipleChildren(children, context);
-  return await traverseUpward(
-    childBranchName,
-    context,
-    stepsRemaining === 'top' ? 'top' : stepsRemaining - 1,
-    indent + 1
-  );
-}
-
-async function handleMultipleChildren(children: string[], context: TContext) {
-  if (!context.interactive) {
-    throw new ExitFailedError(
-      `Cannot get upstack branch in non-interactive mode; multiple choices available:\n${children.join(
-        '\n'
-      )}`
-    );
-  }
-  return (
-    await prompts(
-      {
-        type: 'select',
-        name: 'value',
-        message:
-          'Multiple branches found at the same level. Select a branch to guide the navigation',
-        choices: children.map((b) => {
-          return { title: b, value: b };
-        }),
-      },
-      {
-        onCancel: () => {
-          throw new KilledError();
-        },
-      }
-    )
-  ).value;
-}
-
 type TBranchNavigation =
   | {
       direction: 'UP' | 'DOWN';
@@ -100,6 +14,7 @@ export async function switchBranchAction(
   context: TContext
 ): Promise<void> {
   const currentBranchName = context.metaCache.currentBranchPrecondition;
+  context.splog.logInfo(chalk.blueBright(currentBranchName));
   const newBranchName = await traverseBranches(
     branchNavigation,
     currentBranchName,
@@ -133,7 +48,7 @@ async function traverseBranches(
       return traverseDownward(
         fromBranchName,
         context,
-        branchNavigation.numSteps
+        branchNavigation.numSteps > 1 ? branchNavigation.numSteps : 1
       );
     }
     case 'TOP': {
@@ -143,8 +58,84 @@ async function traverseBranches(
       return await traverseUpward(
         fromBranchName,
         context,
-        branchNavigation.numSteps
+        branchNavigation.numSteps > 1 ? branchNavigation.numSteps : 1
       );
     }
   }
+}
+
+function traverseDownward(
+  currentBranchName: string,
+  context: TContext,
+  stepsRemaining: number | 'bottom' = 'bottom'
+): string {
+  if (stepsRemaining === 0 || context.metaCache.isTrunk(currentBranchName)) {
+    return currentBranchName;
+  }
+  const parentBranchName =
+    context.metaCache.getParentPrecondition(currentBranchName);
+  if (
+    stepsRemaining === 'bottom' &&
+    context.metaCache.isTrunk(parentBranchName)
+  ) {
+    return currentBranchName;
+  }
+  context.splog.logInfo('⮑  ' + parentBranchName);
+  return traverseDownward(
+    parentBranchName,
+    context,
+    stepsRemaining === 'bottom' ? 'bottom' : stepsRemaining - 1
+  );
+}
+
+async function traverseUpward(
+  currentBranchName: string,
+  context: TContext,
+  stepsRemaining: number | 'top' = 'top'
+): Promise<string> {
+  if (stepsRemaining === 0) {
+    return currentBranchName;
+  }
+  const children = context.metaCache.getChildren(currentBranchName);
+  if (children.length === 0) {
+    return currentBranchName;
+  }
+  const childBranchName =
+    children.length === 1
+      ? children[0]
+      : await handleMultipleChildren(children, context);
+  context.splog.logInfo('⮑  ' + childBranchName);
+  return await traverseUpward(
+    childBranchName,
+    context,
+    stepsRemaining === 'top' ? 'top' : stepsRemaining - 1
+  );
+}
+
+async function handleMultipleChildren(children: string[], context: TContext) {
+  if (!context.interactive) {
+    throw new ExitFailedError(
+      `Cannot get upstack branch in non-interactive mode; multiple choices available:\n${children.join(
+        '\n'
+      )}`
+    );
+  }
+  return (
+    await prompts(
+      {
+        type: 'select',
+        name: 'value',
+        message:
+          'Multiple branches found at the same level. Select a branch to guide the navigation',
+        choices: children.map((b) => {
+          return { title: b, value: b };
+        }),
+      },
+      {
+        onCancel: () => {
+          throw new KilledError();
+        },
+      }
+    )
+  ).value;
 }
