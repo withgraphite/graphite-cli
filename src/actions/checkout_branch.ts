0d727a90644fc18aecf1342fa0228c0909249dd9
Jacob Gold <jacob@graphite.dev>
2022-06-02 15:30:02 -0400

    feat: move interactive branch selection to new log short display

--- src/actions/checkout_branch.ts
+++ src/actions/checkout_branch.ts
@@ -4,6 +4,7 @@ import { TContext } from '../lib/context';
 export function checkoutBranch(branchName: string, context: TContext): void {
   if (branchName === context.metaCache.currentBranch) {
     context.splog.logInfo(`Already on ${chalk.cyan(branchName)}.`);
+    return;
   }
   context.metaCache.checkoutBranch(branchName);
   context.splog.logInfo(`Checked out ${chalk.cyan(branchName)}.`);
