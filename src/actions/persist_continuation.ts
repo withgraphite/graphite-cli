eed70e99c30ba8d18fb587061212e8dac914adf9
Jacob Gold <jacob@graphite.dev>
2022-06-03 07:59:30 -0400

    refactor: support persisting branches to sync to a continuation

---
+++ src/actions/persist_continuation.ts
@@ -0,0 +1,36 @@
+import { TContext } from '../lib/context';
+
+export function persistContinuation(
+  args: { branchesToRestack?: string[]; branchesToSync?: string[] },
+  context: TContext
+): void {
+  const [branchesToRestack, branchesToSync] = [
+    args.branchesToRestack ?? [],
+    args.branchesToSync ?? [],
+  ];
+  context.splog.logDebug(
+    branchesToSync.reduce(
+      (acc, curr) => `${acc}\n${curr}`,
+      'PERSISTING (sync):'
+    )
+  );
+  context.splog.logDebug(
+    branchesToRestack.reduce(
+      (acc, curr) => `${acc}\n${curr}`,
+      'PERSISTING (restack):'
+    )
+  );
+  context.continueConfig.update((data) => {
+    data.branchesToSync = branchesToSync;
+    data.branchesToRestack = branchesToRestack;
+    data.currentBranchOverride = context.metaCache.currentBranch;
+  });
+}
+
+export function clearContinuation(context: TContext): void {
+  context.continueConfig.update((data) => {
+    data.branchesToSync = [];
+    data.branchesToRestack = [];
+    data.currentBranchOverride = undefined;
+  });
+}
