ffc1bc4eebcd1b4cb6392b4b1ef71b57c29f2b51
Jacob Gold <jacob@graphite.dev>
2022-06-03 23:38:18 -0500

    refactor: light cleanup on cache typing

--- src/lib/engine/persist_cache.ts
+++ src/lib/engine/persist_cache.ts
@@ -1,9 +1,8 @@
 import { getBranchNamesAndRevisions } from '../git/sorted_branch_names';
-import { cuteString } from '../utils/cute_string';
 import { gpExecSync } from '../utils/exec_sync';
 import { TSplog } from '../utils/splog';
 import { TCachedMeta } from './cached_meta';
-import { CACHE_CHECK_REF, CACHE_DATA_REF } from './cache_loader';
+import { CACHE_CHECK_REF, CACHE_DATA_REF, TCacheKey } from './cache_loader';
 import { getMetadataRefList } from './metadata_ref';
 
 export function persistCache(
@@ -14,12 +13,12 @@ export function persistCache(
   splog.logDebug(`Persisting cache checksum to ${CACHE_CHECK_REF}...`);
   gpExecSync(
     {
-      command: `git update-ref ${CACHE_CHECK_REF} ${hashState(
-        cuteString({
+      command: `git update-ref ${CACHE_CHECK_REF} ${hashCacheOrKey(
+        {
           trunkName: trunkName,
           gitBranchNamesAndRevisions: getBranchNamesAndRevisions(),
           metadataRefList: getMetadataRefList(),
-        }),
+        },
         true
       )}`,
     },
@@ -29,20 +28,23 @@ export function persistCache(
   );
   splog.logDebug(`Persisting cache data to ${CACHE_DATA_REF}...`);
   gpExecSync({
-    command: `git update-ref ${CACHE_DATA_REF} ${hashState(
-      cuteString(cachedBranches),
+    command: `git update-ref ${CACHE_DATA_REF} ${hashCacheOrKey(
+      cachedBranches,
       true
     )}`,
   });
   splog.logDebug(`Persisted cache`);
 }
 
-export function hashState(state: string, write?: boolean): string {
+export function hashCacheOrKey(
+  state: TCacheKey | Record<string, TCachedMeta>,
+  write?: boolean
+): string {
   return gpExecSync(
     {
       command: `git hash-object ${write ? '-w' : ''} --stdin`,
       options: {
-        input: state,
+        input: JSON.stringify(state),
       },
     },
     (err) => {
