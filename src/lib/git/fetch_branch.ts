4998e5da8b17ddda84eab63331c785bfb30816af
Jacob Gold <jacob@graphite.dev>
2022-06-03 23:52:52 -0500

    refactor: fetch head to our own ref instead of git's

--- src/lib/git/fetch_branch.ts
+++ src/lib/git/fetch_branch.ts
@@ -1,20 +1,28 @@
 import { gpExecSync } from '../utils/exec_sync';
+import { getShaOrThrow } from './get_sha';
 
+const FETCH_HEAD = 'refs/gt-metadata/FETCH_HEAD';
+const FETCH_BASE = 'refs/gt-metadata/FETCH_BASE';
 export function fetchBranch(remote: string, branchName: string): void {
-  gpExecSync({ command: `git fetch -q  ${remote} ${branchName}` }, (err) => {
-    throw err;
-  });
+  gpExecSync(
+    {
+      command: `git fetch --no-write-fetch-head -q  ${remote} +${branchName}:${FETCH_HEAD}`,
+    },
+    (err) => {
+      throw err;
+    }
+  );
 }
 export function readFetchHead(): string {
-  return gpExecSync({ command: `git rev-parse FETCH_HEAD` });
+  return getShaOrThrow(FETCH_HEAD);
 }
 
 export function readFetchBase(): string {
-  return gpExecSync({ command: `git rev-parse FETCH_BASE` });
+  return getShaOrThrow(FETCH_BASE);
 }
 
 export function writeFetchBase(sha: string): void {
-  gpExecSync({ command: `git update-ref FETCH_BASE ${sha}` }, (err) => {
+  gpExecSync({ command: `git update-ref ${FETCH_BASE} ${sha}` }, (err) => {
     throw err;
   });
 }
