04d3ffabfbbd29a990e1480fac473657bf6d8aa7
Jacob Gold <jacob@graphite.dev>
2022-06-04 23:36:31 -0500

    fix: remove context dependency for survey bg process

--- src/lib/telemetry/survey/post_survey.ts
+++ src/lib/telemetry/survey/post_survey.ts
@@ -1,4 +1,9 @@
-import { initContext, TContext } from '../../context';
+import graphiteCLIRoutes from '@withgraphite/graphite-cli-routes';
+import { request } from '@withgraphite/retyped-routes';
+import { API_SERVER } from '../../api/server';
+import { surveyConfigFactory } from '../../config/survey_config';
+import { userConfigFactory } from '../../config/user_config';
+import { TContext } from '../../context';
 import { spawnDetached } from '../../utils/spawn';
 
 // We try to post the survey response right after the user takes it, but in
@@ -11,15 +16,43 @@ export function postSurveyResponsesInBackground(context: TContext): void {
   }
 }
 
-export async function postSurveyResponse(context: TContext): Promise<void> {
-  const responsePostedSuccessfully = await context.surveyConfig.postResponses(
-    context
-  );
-  if (responsePostedSuccessfully) {
-    context.surveyConfig.clearPriorSurveyResponses();
+export async function postSurveyResponse(): Promise<void> {
+  try {
+    const surveyConfig = surveyConfigFactory.loadIfExists();
+    const surveyResponse = surveyConfig?.data.responses;
+    const authToken = userConfigFactory.loadIfExists()?.data.authToken;
+
+    if (!surveyConfig || !surveyResponse || !authToken) {
+      return;
+    }
+
+    const response = await request.requestWithArgs(
+      API_SERVER,
+      graphiteCLIRoutes.surveyResponse,
+      {
+        authToken: authToken,
+        responses: {
+          timestamp: surveyResponse.timestamp,
+          responses: surveyResponse.responses.map((qa) => {
+            return {
+              question: qa.question,
+              response: qa.answer,
+            };
+          }),
+          exitedEarly: surveyResponse.exitedEarly,
+        },
+      }
+    );
+
+    if (response._response.status === 200) {
+      surveyConfig.clearPriorSurveyResponses();
+    }
+  } catch (e) {
+    // Ignore any background errors posting the survey; if posting fails,
+    // then we'll try again the next time a user runs a CLI command.
   }
 }
 
 if (process.argv[1] === __filename) {
-  void postSurveyResponse(initContext());
+  void postSurveyResponse();
 }
