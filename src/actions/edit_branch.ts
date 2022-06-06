f840fe3415669edc29ffcf24b4a554a0dc7fff4d
Jacob Gold <jacob@graphite.dev>
2022-06-06 11:24:15 -0700

    fix: limit branch edit to restacked branches to prevent bug

--- src/actions/edit_branch.ts
+++ src/actions/edit_branch.ts
@@ -1,12 +1,21 @@
 import chalk from 'chalk';
 import { TContext } from '../lib/context';
 import { SCOPE } from '../lib/engine/scope_spec';
-import { RebaseConflictError } from '../lib/errors';
+import { ExitFailedError, RebaseConflictError } from '../lib/errors';
 import { persistContinuation } from './persist_continuation';
 import { restackBranches } from './restack';
 
 export function editBranchAction(context: TContext): void {
   const currentBranchName = context.metaCache.currentBranchPrecondition;
+  // TODO fix the bug that breaks interactive rebase on non-restacked branches
+  // (bug is that restack continue sets parent revision to the wrong thing; the
+  // solution will be persisting new parent revision in continuation which also
+  // fixes ds sync rebasing)
+  if (!context.metaCache.isBranchFixed(currentBranchName)) {
+    throw new ExitFailedError(
+      'Can only edit restacked branches in this version, will be fixed soon!'
+    );
+  }
   if (
     context.metaCache.rebaseInteractive(currentBranchName) === 'REBASE_CONFLICT'
   ) {
