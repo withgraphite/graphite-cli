410d87bdc46af0857be01d36ae3364764ae5abef
Jacob Gold <jacob@graphite.dev>
2022-06-07 00:19:04 -0400

    feat: get all metadata refs (#746)
    **Context:**
    
    
    
    **Changes In This Pull Request:**
    
    
    
    **Test Plan:**
    :

--- src/lib/validation/validate.ts
+++ src/lib/validation/validate.ts
@@ -1,5 +1,26 @@
+import { MetadataRef } from '../../wrapper-classes/metadata_ref';
 import { TContext } from '../context';
 
+type TAllBranchRelations = Map<
+  string,
+  { parentName?: string; parentRevision?: string }
+>;
+
 export function validate(context: TContext): void {
-  context.splog.logInfo('validating');
+  const allBranchRelations: TAllBranchRelations = new Map(
+    MetadataRef.allMetadataRefs().map((ref) => {
+      const meta = ref.read();
+      return [
+        ref._branchName,
+        {
+          parentName: meta?.parentBranchName,
+          parentRevision: meta?.parentBranchRevision,
+        },
+      ];
+    })
+  );
+  void context;
+  allBranchRelations.forEach((value, key) => {
+    context.splog.logInfo(`${key}: ${JSON.stringify(value)}`);
+  });
 }
