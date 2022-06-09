09375dccdbbbf32d33b03d043d2059faa8a83894
Jacob Gold <jacob@graphite.dev>
2022-06-08 20:21:25 -0400

    feat: add descriptions for global args (#1004)
    **Context:**
    
    
    
    **Changes In This Pull Request:**
    
    
    
    **Test Plan:**
    :

--- src/lib/global_arguments.ts
+++ src/lib/global_arguments.ts
@@ -2,14 +2,30 @@ import yargs from 'yargs';
 
 export const globalArgumentsOptions = {
   interactive: {
-    alias: 'i',
     default: true,
     type: 'boolean',
     demandOption: false,
+    description: 'Prompt the user. Disable with --no-interactive.',
   },
-  quiet: { alias: 'q', default: false, type: 'boolean', demandOption: false },
-  verify: { default: true, type: 'boolean', demandOption: false },
-  debug: { default: false, type: 'boolean', demandOption: false },
+  quiet: {
+    alias: 'q',
+    default: false,
+    type: 'boolean',
+    demandOption: false,
+    description: 'Minimize output to the terminal.',
+  },
+  verify: {
+    default: true,
+    type: 'boolean',
+    demandOption: false,
+    description: 'Run git hooks. Disable with --no-verify.',
+  },
+  debug: {
+    default: false,
+    type: 'boolean',
+    demandOption: false,
+    description: 'Display debug output.',
+  },
 } as const;
 
 export type TGlobalArguments = Partial<
