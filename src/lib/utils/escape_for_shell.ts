10553618cb724252c7bcee06b76ae0b424caf169
Jacob Gold <jacob@graphite.dev>
2022-06-13 20:07:29 -0400

    fix: node v14 support

--- src/lib/utils/escape_for_shell.ts
+++ src/lib/utils/escape_for_shell.ts
@@ -1,9 +1,10 @@
+const SINGLE_QUOTE_REGEX = /'/g;
 const SINGLE_QUOTE = "'";
 const BACKSLASH = '\\';
 
 export function q(source: string): string {
-  return `${SINGLE_QUOTE}${source.replaceAll(
-    SINGLE_QUOTE,
+  return `${SINGLE_QUOTE}${source.replace(
+    SINGLE_QUOTE_REGEX,
     `${SINGLE_QUOTE}${BACKSLASH}${SINGLE_QUOTE}${SINGLE_QUOTE}`
   )}${SINGLE_QUOTE}`;
 }
