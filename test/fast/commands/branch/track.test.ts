de8e8f16a9811f2c68a16d79b8e0f55df33113a6
jrasmusbm <JRasmusBm@gmail.com>
2022-06-24 21:08:38 +0200

    feat(track): support targeting current branch
    **Why** is the change needed?
    
    For users with existing workflows that create branches it is very handy
    to quickly be able to start tracking the current branch after the fact.
    
    **How** is the need addressed?
    
    - Add test that verifies that the `gt branch track` command can be run
      on an untracked branched without arguments, and that it then tracks
      the current branch.
    - Make the test pass by updating the argument parsing logic according to
      the above feature description.
    
    Concerns / side-effects of the changes:
    
    - This assumes that the feature above is purely a UX change, and not
      something that the underlying system needs to know or care about.

--- test/fast/commands/branch/track.test.ts
+++ test/fast/commands/branch/track.test.ts
@@ -33,5 +33,33 @@ for (const scene of allScenes) {
       scene.repo.execCliCommand('branch down');
       expect(scene.repo.currentBranchName()).to.eq('main');
     });
+
+    it('Can track and restack the current branch if previously untracked', () => {
+      // Create our dangling branch
+      scene.repo.createAndCheckoutBranch('a');
+      scene.repo.createChangeAndCommit('a1', 'a1');
+      scene.repo.createChangeAndCommit('a2', 'a2');
+      scene.repo.createChangeAndCommit('a3', 'a3');
+
+      // Move main forward
+      scene.repo.checkoutBranch('main');
+      scene.repo.createChangeAndCommit('b', 'b');
+
+      // we should be able to track the dangling branch 'a' while it's checked out
+      scene.repo.checkoutBranch('a');
+      expect(() => {
+        scene.repo.execCliCommand('branch track');
+      }).to.not.throw();
+
+      expectCommits(scene.repo, 'a3, a2, a1, 1');
+
+      scene.repo.execCliCommand('branch restack');
+
+      expectCommits(scene.repo, 'a3, a2, a1, b, 1');
+
+      // Prove that we have meta now.
+      scene.repo.execCliCommand('branch down');
+      expect(scene.repo.currentBranchName()).to.eq('main');
+    });
   });
 }
