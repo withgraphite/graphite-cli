5fd52f9901a47bed5b53eae9b8b26e7513f320ad
Jacob Gold <jacob@graphite.dev>
2022-06-30 11:34:22 -0400

    feat: allow gt dpr for dash pr

--- src/lib/pre-yargs/preprocess_command.ts
+++ src/lib/pre-yargs/preprocess_command.ts
@@ -3,24 +3,21 @@ import { passthrough } from './passthrough';
 
 function splitShortcuts(command: string): string[] {
   if (
-    typeof command === 'string' &&
-    command.length == 2 &&
+    command.length === 2 &&
     !['ds', 'us'].includes(command) // block list two letter noun aliases
   ) {
     return [command[0], command[1]];
   }
 
   if (
-    typeof command === 'string' &&
-    command.length == 3 &&
-    ['bco', 'bdl', 'btr', 'but', 'brn'].includes(command) // special case two-letter shortcuts
+    command.length === 3 &&
+    ['bco', 'bdl', 'btr', 'but', 'brn', 'dpr'].includes(command) // special case three-letter shortcuts
   ) {
     return [command[0], command.slice(1)];
   }
 
   if (
-    typeof command === 'string' &&
-    command.length == 3 &&
+    command.length === 3 &&
     ['ds', 'us'].includes(command.slice(0, 2)) // special case two-letter noun aliases
   ) {
     return [command.slice(0, 2), command[2]];
