eb99176552f5a5f903663c36db96f6ffcb9bb439
Jacob Gold <jacob@graphite.dev>
2022-07-07 14:49:26 -0400

    fix: bug with not actually changing name on rename (#1117)
    **Context:**
    
    **Changes In This Pull Request:**
    
    **Test Plan:**

--- test/fast/commands/branch/branch_rename.test.ts
+++ test/fast/commands/branch/branch_rename.test.ts
@@ -29,5 +29,19 @@ for (const scene of allScenes) {
       scene.repo.execCliCommand(`branch down --no-interactive`);
       expect(scene.repo.currentBranchName()).to.equal('main');
     });
+    it("Renaming a branch to its own name doesn't break", () => {
+      scene.repo.createChange('a', 'a');
+      scene.repo.execCliCommand(`branch create "a" -m "a" -q`);
+
+      scene.repo.createChange('b', 'b');
+      scene.repo.execCliCommand(`branch create "b" -m "b" -q`);
+
+      scene.repo.checkoutBranch('a');
+      scene.repo.execCliCommand(`branch rename a`);
+
+      expect(() => scene.repo.execCliCommand(`log short`)).not.to.throw();
+      expect(() => scene.repo.execCliCommand(`branch up`)).not.to.throw();
+      expectCommits(scene.repo, 'b, a, 1');
+    });
   });
 }
