6fc2933b0c510e7b853c480de4dbec467d5b2158
Jacob Gold <jacob@graphite.dev>
2022-07-08 16:02:17 -0400

    feat: split boilerplate

--- src/lib/pre-yargs/preprocess_command.ts
+++ src/lib/pre-yargs/preprocess_command.ts
@@ -11,7 +11,7 @@ function splitShortcuts(command: string): string[] {
 
   if (
     command.length === 3 &&
-    ['bco', 'bdl', 'btr', 'but', 'brn', 'bsq', 'dpr'].includes(command) // special case three-letter shortcuts
+    ['bco', 'bdl', 'btr', 'but', 'brn', 'bsq', 'bsp', 'dpr'].includes(command) // special case three-letter shortcuts
   ) {
     return [command[0], command.slice(1)];
   }
