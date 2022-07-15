f94c59f80f6bea0f6ec880103d8d48593cff2217
Jacob Gold <jacob@graphite.dev>
2022-07-14 21:34:52 -0400

    refactor: move all branch ops into one file

--- src/lib/git/branch_move.ts
+++
@@ -1,9 +0,0 @@
-import { q } from '../utils/escape_for_shell';
-import { gpExecSync } from '../utils/exec_sync';
-
-export function branchMove(newName: string): void {
-  gpExecSync({
-    command: `git branch -m ${q(newName)}`,
-    onError: 'throw',
-  });
-}
