e4544e55bb13a601e7f07414771544748ae7312c
Jacob Gold <jacob@graphite.dev>
2022-07-14 20:42:24 -0400

    fix: remove all stdio ignores

--- test/lib/utils/fake_squash_and_merge.ts
+++ test/lib/utils/fake_squash_and_merge.ts
@@ -8,7 +8,7 @@ export function fakeGitSquashAndMerge(
 ): void {
   // Fake github squash and merge
   execSync(`git -C "${repo.dir}" switch -q -c temp ${branchName}`);
-  execSync(`git -C "${repo.dir}" rebase main -Xtheirs`, { stdio: 'ignore' });
+  execSync(`git -C "${repo.dir}" rebase main -Xtheirs`);
   execSync(
     `git -C "${repo.dir}" reset --soft $(git -C "${repo.dir}" merge-base HEAD main)`
   );
