ea5183e2c3aa1e3e417f11ddfbe2ad5a86db0ab7
Jacob Gold <jacob@graphite.dev>
2022-07-19 21:26:19 -0400

    chore(bump): 0.20.1 (#1207)
    **Context:**
    
    **Changes In This Pull Request:**
    
    **Test Plan:**

--- scripts/node_version.js
+++ scripts/node_version.js
@@ -1,10 +1,14 @@
 /* eslint-disable */
 const requiredVersion = '>=v14';
-if (!require('semver').satisfies(process.version, requiredVersion)) {
-  console.error(
-    require('chalk').red(
-      `Required Node.js version ${requiredVersion} not satisfied with current version ${process.version}.`
-    )
-  );
-  process.exit(1);
+try {
+  if (!require('semver')?.satisfies(process.version, requiredVersion)) {
+    console.error(
+      require('chalk').red(
+        `Required Node.js version ${requiredVersion} not satisfied with current version ${process.version}.`
+      )
+    );
+    process.exit(1);
+  }
+} catch {
+  //pass
 }
