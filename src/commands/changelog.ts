5b1193f36934bd934063396acda472f8436a87e1
Jacob Gold <jacob@graphite.dev>
2022-07-28 14:30:26 -0400

    feat: add pager for changelog (#1230)
    **Context:**
    
    **Changes In This Pull Request:**
    
    **Test Plan:**

--- src/commands/changelog.ts
+++ src/commands/changelog.ts
@@ -14,7 +14,7 @@ export const description = 'Show the Graphite CLI changelog.';
 export const builder = args;
 export const handler = async (argv: argsT): Promise<void> =>
   graphiteWithoutRepo(argv, canonical, async (context) => {
-    context.splog.info(
+    context.splog.page(
       fs.readFileSync(path.join(__dirname, '..', '..', '.CHANGELOG.md'), {
         encoding: 'utf-8',
       })
